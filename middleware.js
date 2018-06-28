d8f3cf0db2508bcda446ef49bd89856c15ec6d89
Randall Degges <r@rdegges.com>
2018-06-27 20:04:43 -0700

    Fixing typo.

--- middleware.js
+++ middleware.js
@@ -2,7 +2,7 @@ const auth = require("./auth");
 
 
 // Tack a user object onto each request if possible
-function addUser(req, res, next) => {
+function addUser(req, res, next) {
   if (!req.userinfo) {
     return next();
   }
